000aeb4db4d8f6a8a499a81a81b11d1a8d1144a5
dork7 <weeday425@gmail.com>
2022-03-17 19:01:01 +0500

    lock deleted

--- src/api/routes/redis.route.js
+++ src/api/routes/redis.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getData, storeData } = require("../controllers/redis.controller");
+// const { getData, storeData } = require("../controllers/redis.controller");
 
 const router = express.Router();
 
