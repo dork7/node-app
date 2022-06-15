93e50421428e15e6ae14bd9e57849691e05f0e57
dork7 <weeday425@gmail.com>
2022-06-15 17:40:12 +0500

    shit changes

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import packages
+const express = require('express');
+const home = require('./routes/home');
+
+// Middlewares
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/home', (req, res) => {
+  return res.status(200).json({
+    title: 'Express Testing',
+    message: 'The app is working properly!',
+  });
+});
+
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
