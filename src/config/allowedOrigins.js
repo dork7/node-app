28f89c58c4e88c3fe658aa1ac2eed8bcbe474eee
Hamza Mehmood Anwar <hamzamehmood.anwar@systemsltd.com>
2023-03-02 15:52:14 +0500

    origin added

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:4000',
-  'https://react-demo.dork7.tk/',
+  'https://react-demo.dork7.tk',
 ];
 
 module.exports = allowedOrigins;
