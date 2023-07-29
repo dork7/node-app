acefa46ea438fd95d26e73e395454071e0023c92
Hamza Mehmood <54894721+dork7@users.noreply.github.com>
2023-07-29 18:08:18 +0400

    cors update

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:4000',
   'https://react-demo.dork7.tk',
+  'http://localhost:5173',
+  'https://cv.dork7.tk'
 ];
 
 module.exports = allowedOrigins;
