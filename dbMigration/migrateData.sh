c63b0b19185c50f774c95a2fe8e8dc8d7df14137
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-08-21 12:16:15 +0400

    docker file and compose file for db migration

---
+++ dbMigration/migrateData.sh
@@ -0,0 +1,3 @@
+mongodump --uri="mongodb+srv://dork7:vUe9WHLa0TiQCcjJ@mcluster.7kxtv.mongodb.net/node_app?retryWrites=true&w=majority" --out=/mongodb_data
+
+mongorestore --nsInclude=node_app.* /mongodb_data
