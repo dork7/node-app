4b507ad09e34afda759bb3e7be0872801f6d77a2
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-08-24 15:32:21 +0400

    added support for multiple emails

--- src/api/controllers/mail/mail.controller.js
+++ src/api/controllers/mail/mail.controller.js
@@ -4,17 +4,17 @@ const mailModel = require("../../models/mail.model");
 
 exports.sendMail = async (req, res, next) => {
   try {
-    const mailRes = await sendMail({
-      email: Array.isArray(req.body.email) ? req.body.email.join() : req.body.email,
-      subject: req.body.subject ?? '',
-      mailBody: req.body.mailBody,
-    });
-    // const storedMail = await this.storeEmails(req.body)
+    // const mailRes = await sendMail({
+    //   email: Array.isArray(req.body.email) ? req.body.email.join() : req.body.email,
+    //   subject: req.body.subject ?? '',
+    //   mailBody: req.body.mailBody,
+    // });
+    const storedMail = await this.storeEmails(req.body)
     // info({ message: mailRes });
     return res.status(200).json({
       success: true,
-      mailRes,
-      // storedMail
+      // mailRes,
+      storedMail
     });
   } catch (error) {
     return next(error);
@@ -24,9 +24,17 @@ exports.sendMail = async (req, res, next) => {
 exports.storeEmails = async (emailDataSet) => {
   try {
     const { email, subject, mailBody } = emailDataSet
-    const mailSaved = await new mailModel(emailDataSet).save()
-    console.log('mailSaved', mailSaved)
-    return mailSaved
+    if (!Array.isArray(email)) {
+      const mailSaved = await new mailModel(emailDataSet).save()
+      return mailSaved
+    }
+    else {
+      // bulk add
+      const dataSet = email.map(email => {return { email, subject, mailBody }})
+      console.log('dataSet', dataSet)
+      const mailSaved = await new mailModel.insertMany(dataSet)
+      return mailSaved
+    }
 
   } catch (error) {
     return error
