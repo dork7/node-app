0122d6dc1b3b9e1131e976d6c7806559515f6dcc
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-08-24 08:26:38 +0400

    data migration docker compose

--- dbMigration/migrateData.sh
+++ dbMigration/migrateData.sh
@@ -1,3 +1,5 @@
 mongodump --uri="mongodb+srv://dork7:vUe9WHLa0TiQCcjJ@mcluster.7kxtv.mongodb.net/node_app?retryWrites=true&w=majority" --out=/mongodb_data
 
-mongorestore --nsInclude=node_app.* /mongodb_data
+# mongorestore --nsInclude=node_app.* /mongodb_data
+
+mongorestore --host db --port 27017  /mongodb_data
