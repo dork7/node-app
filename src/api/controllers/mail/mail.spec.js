79086b15d3f7f652f3f2608b7758aa5c3a9e062e
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-08-25 11:25:26 +0400

    chore : faker added in mail test

--- src/api/controllers/mail/mail.spec.js
+++ src/api/controllers/mail/mail.spec.js
@@ -3,10 +3,7 @@ const app = require("../../../config/express");
 const mongoose = require("mongoose");
 
 require("../../../config/express");
-
-
-let reqRefreshToken = null;
-
+const { faker } = require('@faker-js/faker');
 
 /* Connecting to the database before each test. */
 beforeEach(async () => {
@@ -19,23 +16,34 @@ afterEach(async () => {
     await mongoose.connection.close();
 });
 
+function createRandomUser() {
+    return {
+        email: faker.internet.email(),
+    };
+}
 
 describe("Email sending || storign in db", () => {
-    it("API: /v1/mail", async () => {
+
+
+    const emails = faker.helpers.multiple(createRandomUser, {
+        count: 2,
+    }).map(item => item.email).concat('hamzameh122@gmail.com')
+
+     it("API: /v1/mail", async () => {
         const res = await request(app).post("/v1/mail").send({
-            "email": ["hamzameh122@gmail.com" , "hamxa2266@gmail.com"],
+            "email": emails,
             "subject": "Testing email from jest",
-            "mailBody": "Dear  <p>This is a testing mail </p>  <p>Checking content </p> <br>  <p>Please dont do this </p> <h2>lol </h2>"
+            "mailBody": faker.lorem.lines() //"Dear  <p>This is a testing mail </p>  <p>Checking content </p> <br>  <p>Please dont do this </p> <h2>lol </h2>"
         });
-        const { success, data : {accepted} } = res.body;
+        const { success, data: { accepted } } = res.body;
         expect(res.statusCode).toBe(200);
         expect(success).toBe(true);
         expect(accepted.length).toBeGreaterThan(0)
     });
 
     it("API - GET: /v1/mail", async () => {
-        const res = await request(app).get(`/v1/mail/hamzameh122@gmail.com`);
-        const { success, data  } = res.body;
+        const res = await request(app).get(`/v1/mail/${emails[0]}`);
+        const { success, data } = res.body;
         expect(res.statusCode).toBe(200);
         expect(success).toBe(true);
         expect(data.length).toBeGreaterThan(0)
