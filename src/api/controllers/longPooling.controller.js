90941d2bc6e6185ae447741473c305358b543482
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2024-02-07 08:17:21 +0400

    long poolong fixed

--- src/api/controllers/longPooling.controller.js
+++ src/api/controllers/longPooling.controller.js
@@ -2,7 +2,7 @@ const { success, error, info } = require('consola');
 const { connection } = require('mongoose');
 
 let connections = [];
-const LIMIT = 200;
+const LIMIT = 5;
 const DELAY = 1000;
 exports.longPooling = async (req, res, next) => {
   try {
@@ -20,17 +20,20 @@ exports.longPooling = async (req, res, next) => {
 let tick = 0;
 
 setTimeout(function run() {
-  console.log('tick', tick);
-  if (++tick > LIMIT) {
-    connections.map((res) => {
-      res.write('END\n');
-      res.end();
+  if (connections.length > 0) {
+    console.log('Long pooling - tick', tick);
+    if (++tick > LIMIT) {
+      connections.map((res, idx) => {
+        connections[idx] = null
+        res.write('END\n');
+        res.end();
+      });
+      connections = [];
+      tick = 0;
+    }
+    connections.map((res, idx) => {
+      res.write(`Hello response -> ${tick}\n`);
     });
-    connections = [];
-    tick = 0;
   }
-  connections.map((res) => {
-    res.write(`Hello response -> ${tick}\n`);
-  });
   setTimeout(run, DELAY);
 }, DELAY);
